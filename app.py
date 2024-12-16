6f661bc058d1cb7d21cbe755bad4e0b3d1503efb
ggeromedina <gmedinam@fi.uba.ar>
2024-12-16 13:42:14 -0300

    Crea la instancia de Flask, configura la conexión a PostgreSQL y desactiva el seguimiento de modificaciones

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 from flask import Flask, jsonify, request, abort, send_file
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://proyecto4:1234@db:5432/recudadacion_fondos'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
