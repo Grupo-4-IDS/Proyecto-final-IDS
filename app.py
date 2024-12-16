4c985be45bbc8037995110f058769127e91b9d77
ggeromedina <gmedinam@fi.uba.ar>
2024-12-16 13:50:00 -0300

    agrego la configuracion de SQAlchemy para manejar la base de datos en flask

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://proyecto4:1234@db:5432/recudadacion_fondos'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
