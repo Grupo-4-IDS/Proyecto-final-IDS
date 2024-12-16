cdab6e81651553d68483a86130eaea41c893d929
ggeromedina <gmedinam@fi.uba.ar>
2024-12-16 18:33:51 -0300

    se crea una clase para los usuarios, que representa la tabla 'usuarios' de la base de datos usando SQLAlchemy

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://proyecto4:1234@db:5432/rec
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    cuit_cuil = db.Column(db.String(20), nullable=False, unique=True)
+    contrasena = db.Column(db.String(10), nullable=False)
+    fecha_registro = db.Column(db.Date, nullable=False, default=db.func.current_date)
