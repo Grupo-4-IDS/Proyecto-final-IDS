1aab524c0a43a1ce5123ecdd05d85c4468a8ee87
martinafanelli <mfanelli@fi.uba.ar>
2024-12-18 11:51:54 -0300

    creo tabla donaciones

--- psql_data.sql
+++ psql_data.sql
@@ -15,3 +15,12 @@ CREATE TABLE Causas (
     foto TEXT,
     monto_recaudado NUMERIC(12, 2) DEFAULT 0
 );
+
+CREATE TABLE Donaciones (
+    ID SERIAL PRIMARY KEY,
+    monto NUMERIC(11, 2) NOT NULL,
+    fecha DATE NOT NULL DEFAULT CURRENT_DATE,
+    forma_pago VARCHAR(20) NOT NULL,
+    id_usuario INT REFERENCES Usuarios(ID) ON DELETE CASCADE, 
+    id_causa INT REFERENCES Causas(ID) ON DELETE CASCADE
+);
