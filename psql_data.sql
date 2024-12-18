52be25df601f8f8ee7dfd60a3577620e710692d8
martinafanelli <mfanelli@fi.uba.ar>
2024-12-18 11:43:32 -0300

    creo tabla usuarios

--- psql_data.sql
+++ psql_data.sql
@@ -6,22 +6,3 @@ CREATE TABLE Usuarios (
     contrasena VARCHAR(10) NOT NULL,
     fecha_registro DATE NOT NULL DEFAULT CURRENT_DATE
 );
-
-CREATE TABLE Causas (
-    ID SERIAL PRIMARY KEY,
-    fecha_creacion DATE NOT NULL DEFAULT CURRENT_DATE,
-    descripcion TEXT NOT NULL,
-    meta NUMERIC(11, 2) NOT NULL,
-    foto TEXT,
-    monto_recaudado NUMERIC(12, 2) DEFAULT 0
-);
-
-
-CREATE TABLE Donaciones (
-    ID SERIAL PRIMARY KEY,
-    monto NUMERIC(11, 2) NOT NULL,
-    fecha DATE NOT NULL DEFAULT CURRENT_DATE,
-    forma_pago VARCHAR(20) NOT NULL,
-    id_usuario INT REFERENCES Usuarios(ID) ON DELETE CASCADE, 
-    id_causa INT REFERENCES Causas(ID) ON DELETE CASCADE
-);
